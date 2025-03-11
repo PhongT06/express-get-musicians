37076c2bb9abaea82b2097d2bde932237781b6e1
PhongT06 <phongtt11@yahoo.com>
2025-03-11 17:05:37 -0500

    added post, put, get, delete and testing

--- index.test.js
+++ index.test.js
@@ -9,9 +9,7 @@ const { Musician } = require('./models/index')
 const app = require('./src/app');
 const {seedMusician} = require("./seedData");
 
-
-describe('./musicians endpoint', () => {
-    // Write your tests here
+describe('GET /musicians endpoint', () => {
     test('should return all musicians with status code 200', async () => {
         const response = await request(app).get('/musicians');
         expect(response.statusCode).toBe(200);
@@ -19,4 +17,50 @@ describe('./musicians endpoint', () => {
         expect(Array.isArray(responseData)).toBe(true);
         expect(responseData.length).toBeGreaterThan(0);
     });
-})
+});
+
+describe('GET /musicians/:id', () => { 
+    test('should return musician with id 1 and status 200', async () => {
+        const response = await request(app).get('/musicians/1');
+        expect(response.statusCode).toBe(200);
+        const responseData = JSON.parse(response.text);
+        expect(responseData).toHaveProperty('id', 1);
+        expect(responseData).toHaveProperty('name', 'Mick Jagger');
+    });
+});
+
+describe('POST /musicians', () => {
+    test('should create a new musician and return 200 status', async () => {
+        const newMusician = { name: 'Taylor Swift', instrument: 'Guitar' };
+        const response = await request(app)
+            .post('/musicians')
+            .send(newMusician);
+        expect(response.statusCode).toBe(200);
+        const responseData = JSON.parse(response.text);
+        expect(responseData).toHaveProperty('name', 'Taylor Swift');
+        expect(responseData).toHaveProperty('instrument', 'Guitar');
+    });
+});
+
+describe('PUT /musicians/:id', () => {
+    test('should update musician with id 1 and return updated data', async () => {
+        const updatedMusician = {
+            name: 'Steven Tyler',
+            instrument: 'Vocals'
+        };
+        const response = await request(app)
+            .put('/musicians/1')
+            .send(updatedMusician);
+        expect(response.statusCode).toBe(200);
+        const responseData = JSON.parse(response.text);
+        expect(responseData).toHaveProperty('name', 'Steven Tyler');
+        expect(responseData).toHaveProperty('instrument', 'Vocals');
+    });
+});
+
+describe('DELETE /musicians/:id', () => {
+    test('should delete musician with id 2 and return 204 status', async () => {
+        const response = await request(app).delete('/musicians/2');
+        expect(response.statusCode).toBe(204);
+    });
+});
--- src/app.js
+++ src/app.js
@@ -5,15 +5,53 @@ const { db } = require("../db/connection")
 
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
 //TODO: Create a GET /musicians route to return all musicians 
 app.get('/musicians', async (req, res) => {
    const musicians = await Musician.findAll();
    res.json(musicians);
 });
 
+app.get('/musicians/:id', async (req, res) => {
+   const id = req.params.id;
+   const musician = await Musician.findByPk(id);
+   res.json(musician);
+});
 
+app.post('/musicians', async (req, res) => {
+   try {
+      const musician = await Musician.create(req.body);
+      res.json(musician);
+   } catch (error) {
+      res.status(400).json('Error creating musician');
+   }
+});
 
+app.put('/musicians/:id', async (req, res) => {
+   try {
+      const id = req.params.id;
+      await Musician.update(req.body, {
+         where: { id: id }
+      })
+      const updatedMusician = await Musician.findByPk(id);
+      res.json(updatedMusician);
+   } catch (error) {
+      res.status(400).json('Error updating musician');
+   }
+});
 
-
+app.delete('/musicians/:id', async (req, res) => {
+   try {
+      const id = req.params.id;
+      await Musician.destroy({
+         where: { id: id }
+      })
+      res.status(204).send();
+   } catch (error) {
+      res.status(400).json('Error deleting musician');
+   }
+});
 
 module.exports = app;
